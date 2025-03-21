0431d31543ed4b5b61bd80be998e25336d25a963
yeeseng_sit <yeeseng.teoh@singaporetech.edu.sg>
2025-03-21 22:08:18 +0800

    UPDATE: files for model training on foodsg233

--- Dev/scripts/backup_llamafactory_gpu0_container.sh
+++ Dev/scripts/backup_llamafactory_gpu0_container.sh
@@ -2,4 +2,4 @@
 cd ~/HealthcareFoodProject/Dev/VLM_Fine_Tuning
 docker cp llamafactory-gpu0:/app/. LLaMA-Factory/.
 cd LLaMA-Factory
-rm -rf custom_data/UECFOOD256 wandb mlruns
+rm -rf custom_data/UECFOOD256 custom_data/foodsg-233_sample wandb mlruns
